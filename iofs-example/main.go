4d10fc40b6bbda4a0e766b5ef74e9266daa9288e
suzuken <suzuken326@gmail.com>
2021-02-27 16:30:13 +0900

    implement fs.FS

--- iofs-example/main.go
+++ iofs-example/main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"io/fs"
+	"os"
+)
+
+type fooFS struct{}
+
+func (fsys *fooFS) Open(name string) (fs.File, error) {
+	return os.Open(name)
+}
+
+func NewFS() fs.FS {
+	return &fooFS{}
+}
+
 func main() {
+	f := NewFS()
+	file, err := f.Open("main.go")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	s, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("s = %+v\n", s)
 }
