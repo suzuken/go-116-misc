f6f1c3e0351b1b15ea63c36f8c711a6513c593f4
suzuken <suzuken326@gmail.com>
2021-02-27 15:23:56 +0900

    embed examples

--- embeded/main.go
+++ embeded/main.go
@@ -1,10 +1,36 @@
 package main
 
-import _ "embed"
+import (
+	"embed"
+	"os"
+	"text/template"
+)
 
 //go:embed hello.txt
 var s string
 
+//go:embed hello.txt
+var b []byte
+
+//go:embed hello.txt
+var f embed.FS
+
+//go:embed template/*.tmpl
+var templates embed.FS
+
 func main() {
 	print(s)
+	print(string(b))
+
+	data, _ := f.ReadFile("hello.txt")
+	print(string(data))
+
+	t, err := template.ParseFS(templates, "template/*")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := t.ExecuteTemplate(os.Stdout, "example.tmpl", "Gopher"); err != nil {
+		panic(err)
+	}
 }
